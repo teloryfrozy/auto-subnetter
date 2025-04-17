b24dab6a35737ba51bf8f5fd467aeef22c5b82b8
teloryfrozy <raptor.frozy@gmail.com>
2025-04-17 22:55:32 +0200

    ⚙️ setup: add network + broadcast IPv4

--- src/routes/ipv4-calculator/+page.svelte
+++ src/routes/ipv4-calculator/+page.svelte
@@ -14,12 +14,6 @@
 
       features:
 display network, broadcast, first, last, nb ip av, subnet
-
-      get_number_ip_available |    first ip av + last
-      get_broadcast
-      get_network
-      get_subnet_mask
-
       */
 
     constructor(ip: string) {
@@ -43,8 +37,57 @@ display network, broadcast, first, last, nb ip av, subnet
       this.nbBitsHostPart = 32 - this.slash;
     }
 
+    getMask(): string {
+      const n = Math.floor(this.slash / 8);
+      let mask = [];
+
+      for (let i = 0; i < n; i++) {
+        mask.push(255);
+      }
+
+      // number of bits for the network part on the (n+1)byte
+      const networkBits =
+        this.nbBitsHostPart % 8 != 0 ? 8 - (this.nbBitsHostPart % 8) : 0;
+
+      // all the network bits are set to ones and the remainding bits are zeroes
+      let nextByte = "1".repeat(networkBits) + "0".repeat(8 - networkBits);
+
+      mask.push(parseInt(nextByte, 2));
+
+      // finish by filling with zeroes the remaining bytes
+      for (let i = n + 2; i < 5; i++) {
+        mask.push(0);
+      }
+
+      return mask.join(".");
+    }
+
     getNbHostsAvailable(): number {
-      return 2 * this.nbBitsHostPart - 2;
+      return 2 ** (32 - this.slash) - 2;
+    }
+
+    getFirstIpAvailable(): string {
+      let firstIpAvailable = [];
+      this.getNetwork()
+        .split(".")
+        .forEach((byte) => firstIpAvailable.push(byte));
+
+      firstIpAvailable[3] = Number(this.getNetwork().split(".")[3]) + 1;
+      firstIpAvailable.forEach((byte) => byte.toString());
+
+      return firstIpAvailable.join(".");
+    }
+
+    getLastIpAvailable(): string {
+      let lastIpAvailable = [];
+      this.getBroadcast()
+        .split(".")
+        .forEach((byte) => lastIpAvailable.push(byte));
+
+      lastIpAvailable[3] = Number(this.getBroadcast().split(".")[3]) - 1;
+      lastIpAvailable.forEach((byte) => byte.toString());
+
+      return lastIpAvailable.join(".");
     }
 
     getBroadcast(): string {
@@ -77,10 +120,41 @@ display network, broadcast, first, last, nb ip av, subnet
 
       return broadcast.join(".");
     }
+
+    getNetwork(): string {
+      // the first n bytes are fixed
+      const n = Math.floor(this.slash / 8);
+
+      let network = [];
+      for (let i = 0; i < n; i++) {
+        network.push(Number(this.ip[i]));
+      }
+      // number of bits for the network part on the (n+1)byte
+      const networkBits =
+        this.nbBitsHostPart % 8 != 0 ? 8 - (this.nbBitsHostPart % 8) : 0;
+
+      // convert the n byte into bin and keep its network part
+      let nextByte = ("00000000" + decimalToBinary(Number(this.ip[n])))
+        .slice(-8)
+        .slice(0, networkBits);
+
+      // fill the rest of the byte part with zeroes
+      for (let i = networkBits + 1; i < 9; i++) {
+        nextByte += "0";
+      }
+      network.push(parseInt(nextByte, 2));
+
+      // finish by filling with 0 the remaining bytes
+      for (let i = n + 2; i < 5; i++) {
+        network.push(0);
+      }
+
+      return network.join(".");
+    }
   }
 
-  const test = new IPv4("192.168.1.2/21");
-  console.log("ip", test, "split", test.slash, test.getBroadcast());
+  const test = new IPv4("192.168.1.1/19");
+  console.log("getMask", test.getMask());
 </script>
 
 <h1>Welcome to SvelteKit</h1>
